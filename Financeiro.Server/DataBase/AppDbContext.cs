4ae5065fe9192d470a14d730d2b45841793609db
Vitor Hugo Belorio Simão <vitorhugobsimao@gmail.com>
2025-05-12 17:36:51 -0300

    :sparkles: feat: Criando os arquivois de configuração para o banco de dados

---
+++ Financeiro.Server/DataBase/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Financeiro.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Financeiro.Server.DataBase
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        // Aqui você pode definir suas DbSets
+        // public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; } = null!;
+        public DbSet<Categoria> Categorias { get; set; } = null!;
+        public DbSet<Transacao> Transacoes { get; set; } = null!;
+    }
+}
